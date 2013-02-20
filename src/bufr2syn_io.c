5435d22e05637858280459bc7d7a7084eb9cf3be
Guillermo Ballester Valor <gbv@ogimet.com>
2013-02-20 06:24:20 +0000

    se arregla desincronización
    git-svn-id: https://localhost/repos/bufr2synop@9 9cae83b6-5e8d-4488-8a60-16aa01b6ee26

--- src/bufr2syn_io.c
+++ src/bufr2syn_io.c
@@ -18,34 +18,37 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 /*!
-  \file bufr2syn_io.c
-  \brief file with the i/o code for binary bufr2synop
-*/
+ \file bufr2syn_io.c
+ \brief file with the i/o code for binary bufr2synop
+ */
 
 #include "bufr2synop.h"
 
-void print_usage(void)
+void
+print_usage(void)
 {
   printf("Usage: \n");
   printf("  bufr2synop -i input [-o output][-v]\n");
-  printf("       -i input. Pathname of the file with the bufr message to parse\n");
-  printf("       -o output. Pathname of output file. Default is standar output\n");
+  printf(
+      "       -i input. Pathname of the file with the bufr message to parse\n");
+  printf(
+      "       -o output. Pathname of output file. Default is standar output\n");
   printf("       -v. Verbose output\n");
   printf("       -h. Show this help\n");
 }
 
-
-int read_bufr(unsigned char *bufr, char *filename, int *length)
+int
+read_bufr(unsigned char *bufr, char *filename, int *length)
 {
   int aux;
   size_t n = 0;
   FILE *fp;
 
   /* Open input file */
-  if ( ( fp = fopen ( filename, "r" ) ) == NULL )
+  if ((fp = fopen(filename, "r")) == NULL )
     {
-      fprintf (stderr, "cannot open file '%s'\n", filename);
-      exit (EXIT_FAILURE);
+      fprintf(stderr, "cannot open file '%s'\n", filename);
+      exit(EXIT_FAILURE);
     }
 
   while ((aux = fgetc(fp)) != EOF && n < *length)
@@ -63,10 +66,11 @@ int read_bufr(unsigned char *bufr, char *filename, int *length)
     return -1;
 
   // check if end with '7777'
-  if (bufr[n - 4] != '7' || bufr[n - 3] != '7' || bufr[n - 2] != '7' || bufr[n - 1] != '7')
+  if (bufr[n - 4] != '7' || bufr[n - 3] != '7' || bufr[n - 2] != '7'
+      || bufr[n - 1] != '7')
     return -2;
 
-  printf("%d %d\n", n , three_bytes_to_uint(bufr + 4));
+  printf("%d %d\n", n, three_bytes_to_uint(bufr + 4));
   // check about the expected size
   if (n != three_bytes_to_uint(bufr + 4))
     return -3;
@@ -75,10 +79,11 @@ int read_bufr(unsigned char *bufr, char *filename, int *length)
 }
 
 /*!
-  \fn int read_arguments( int _argc, char * _argv[])
-  \brief read input arguments with the aid of getopt 
-*/
-int read_arguments( int _argc, char * _argv[])
+ \fn int read_arguments( int _argc, char * _argv[])
+ \brief read input arguments with the aid of getopt
+ */
+int
+read_arguments(int _argc, char * _argv[])
 {
   int iopt;
 
@@ -88,26 +93,26 @@ int read_arguments( int _argc, char * _argv[])
   VERBOSE = 0;
 
   /*
-     Read input arguments using getop library
-  */
-  while ((iopt = getopt(_argc, _argv, "hi:o:v"))!=-1)
+   Read input arguments using getop library
+   */
+  while ((iopt = getopt(_argc, _argv, "hi:o:v")) != -1)
     switch (iopt)
       {
-      case 'i':
-        if (strlen(optarg) < 256)
-          strcpy(INPUTFILE, optarg);
-        break;
-      case 'o':
-        if (strlen(optarg) < 256)
-          strcpy(OUTPUTFILE, optarg);
-        break;
-      case 'v':
-        VERBOSE = 1;
-        break;
-      case 'h':
-      default:
-        print_usage();
-        exit (EXIT_SUCCESS);
+    case 'i':
+      if (strlen(optarg) < 256)
+        strcpy(INPUTFILE, optarg);
+      break;
+    case 'o':
+      if (strlen(optarg) < 256)
+        strcpy(OUTPUTFILE, optarg);
+      break;
+    case 'v':
+      VERBOSE = 1;
+      break;
+    case 'h':
+    default:
+      print_usage();
+      exit(EXIT_SUCCESS);
       }
 
   if (strlen(INPUTFILE) == 0)
--- src/bufr2synop.c
+++ src/bufr2synop.c
@@ -21,61 +21,60 @@
  \file bufr_decode.c
  \brief decodes a bufr file using ECMWF bufr library and tries to pass the decoded reports to synop-format
 
-*/
+ */
 #include "bufr2synop.h"
 
 #define DEBUG
 
 /*!
-  Note that big arrays are defined as global here to avoid segfaults
-*/
+ Note that big arrays are defined as global here to avoid segfaults
+ */
 
 unsigned char BUFR_MESSAGE[BUFR_LEN]; /*!< The array where the bufr file will be stored when readed */
 
 char CNAMES[KELEM][64]; /*!< Array of strings with name of vars */
 char CUNITS[KELEM][24]; /*!< Array of strings with name of used units */
 
-int KSUP[9];  /*!< array containing supplementary information */
+int KSUP[9]; /*!< array containing supplementary information */
 int KSEC0[3]; /*!< array (size 3) containing Bufr Section 0 information.
-                   [0] Length of section 0 in bytes
-                   [1] Total length of Bufr message in bytes
-                   [2] Bufr Edition number (currently 4) */
+ [0] Length of section 0 in bytes
+ [1] Total length of Bufr message in bytes
+ [2] Bufr Edition number (currently 4) */
 int KSEC1[40]; /*!< array of at least 40 words containing Bufr Section 1 information. When Section
-                 1 contains data for local use, KSEC1 should be sized accordingly.
-                   [0] Length of section 1 in bytes
-                   [1] Bufr Edition number (currently 4)
-                   [2] Originating centre
-                   [3] Update sequence number
-                   [4] Flag (presence of Section 2 in the message)
-                   [5] Bufr message type ( Bufr Table A)
-                   [6] Bufr message subtype (local use)
-                   [7] Version number of local table used
-                   [8] Year
-                   [9] Month
-                   [10] Day
-                   [11] Hour
-                   [12] Minute
-                   [13] Bufr Master Table used
-                   [14] Version number of Master table used
-                   [15] Originating sub-centre
-                   [16] International sub-category
-                   [17] Second
-                   [18-39] Local ADP centre information (byte by byte) */
+ 1 contains data for local use, KSEC1 should be sized accordingly.
+ [0] Length of section 1 in bytes
+ [1] Bufr Edition number (currently 4)
+ [2] Originating centre
+ [3] Update sequence number
+ [4] Flag (presence of Section 2 in the message)
+ [5] Bufr message type ( Bufr Table A)
+ [6] Bufr message subtype (local use)
+ [7] Version number of local table used
+ [8] Year
+ [9] Month
+ [10] Day
+ [11] Hour
+ [12] Minute
+ [13] Bufr Master Table used
+ [14] Version number of Master table used
+ [15] Originating sub-centre
+ [16] International sub-category
+ [17] Second
+ [18-39] Local ADP centre information (byte by byte) */
 int KSEC2[4096];/*!< array of 4096 words containing Bufr Section 2 information. ECMWF uses this
-                 section to store Report Data Base KEY.
-                   [0] Length of Section 2 in bytes
-                   [1-4095] Report Data Base KEY in packed form */
-int KSEC3[4];   /*!< array of 4 words containing Bufr Section 3 information.
-                   [0] Length of Section 3 in bytes
-                   [1] Reserved
-                   [2] Number of subsets
-                   [3] Flag (data type, compression) */
-int KSEC4[2];   /*!< array of 2 words containing Section 4 information.
-                   [0] Length of Section 4 in bytes
-                   [1] Reserved */
+ section to store Report Data Base KEY.
+ [0] Length of Section 2 in bytes
+ [1-4095] Report Data Base KEY in packed form */
+int KSEC3[4]; /*!< array of 4 words containing Bufr Section 3 information.
+ [0] Length of Section 3 in bytes
+ [1] Reserved
+ [2] Number of subsets
+ [3] Flag (data type, compression) */
+int KSEC4[2]; /*!< array of 2 words containing Section 4 information.
+ [0] Length of Section 4 in bytes
+ [1] Reserved */
 int KEY[46];
 int KERR; /*!< An INTEGER containing an error code.*/
-
 
 char CVALS[KVALS][80]; /*!< array of strings with value of data */
 
@@ -90,7 +89,8 @@ int VERBOSE; /*!< If != 0 the verbose output */
 
 struct synop_chunks SYN; /*!< struct where to set chunks of synops taken from a bufr subset */
 
-int main ( int argc, char *argv[] )
+int
+main(int argc, char *argv[])
 {
 
   FILE *fp;
@@ -107,132 +107,133 @@ int main ( int argc, char *argv[] )
 
   if (read_arguments(argc, argv))
     {
-      fprintf (stderr, "%s: error geting arguments\n", SELF);
+      fprintf(stderr, "%s: error geting arguments\n", SELF);
       return (EXIT_FAILURE);
     }
 
-
   /* Read in bufr messages */
   //status = readbufr ( fp, &BUFR_MESSAGE, &length );
-  if (status = read_bufr(BUFR_MESSAGE, INPUTFILE, &length))
+  if ((status = read_bufr(BUFR_MESSAGE, INPUTFILE, &length)))
     {
-      if ( status == -1 )
-        fprintf ( stderr, "File '%s' do not begin with 'BUFR'.\n", INPUTFILE );
-      else if ( status == -2 )
-        fprintf ( stderr, "FILE '%s' do not end with '7777'.\n", INPUTFILE );
-      else if ( status == -3 )
-        fprintf ( stderr, "FILE '%s' has different size than expected.\n", INPUTFILE );
-      else if ( status == -4 )
-        fprintf ( stderr, "Too small input FILE.\n" );
-      exit (EXIT_FAILURE);
+      if (status == -1)
+        fprintf(stderr, "File '%s' do not begin with 'BUFR'.\n", INPUTFILE);
+      else if (status == -2)
+        fprintf(stderr, "FILE '%s' do not end with '7777'.\n", INPUTFILE);
+      else if (status == -3)
+        fprintf(stderr, "FILE '%s' has different size than expected.\n",
+            INPUTFILE);
+      else if (status == -4)
+        fprintf(stderr, "Too small input FILE.\n");
+      exit(EXIT_FAILURE);
     }
   else if (VERBOSE)
     {
-      printf ( "It is OK.\n" );
-      printf ( "message read " );
-      printf ( "%d\n", length );
-      printf ( "%s\n", &BUFR_MESSAGE[0] ); // print the string 'BUFR'
+      printf("It is OK.\n");
+      printf("message read ");
+      printf("%d\n", length);
+      printf("%s\n", &BUFR_MESSAGE[0]); // print the string 'BUFR'
     }
 
   /*! Fortran routines works with 4-bytes words, so the readed file byte-oriented
-      is redefined as an array of unsigned int
-  */
-  kbuff = ( unsigned int * ) BUFR_MESSAGE;
+   is redefined as an array of unsigned int
+   */
+  kbuff = (unsigned int *) BUFR_MESSAGE;
   length /= 4; // Now length is sized in words
 
   if (VERBOSE)
     {
 
-      bus012_ ( &length, kbuff , KSUP, KSEC0, KSEC1, KSEC2,  &KERR ) ;
-      buprs0_ ( KSEC0 );
-      buprs1_ ( KSEC1 );
+      bus012_(&length, kbuff, KSUP, KSEC0, KSEC1, KSEC2, &KERR);
+      buprs0_(KSEC0);
+      buprs1_(KSEC1);
 
       // KSUP[5] is the number of subsets
-      if ( KSUP[5] > 1 )
-        kelem = kvals/KSUP[5];
+      if (KSUP[5] > 1)
+        kelem = kvals / KSUP[5];
       else
         kelem = KELEM;
 
-      if ( kelem > KELEM ) kelem = KELEM;
+      if (kelem > KELEM)
+        kelem = KELEM;
     }
 
   /*! Here we call to bufrex() rotine, doing all de decode bufr work
 
    ON INPUT:
 
-     length - An INTEGER variable containing length of Bufr message in words.
-     kbuff - An INTEGER array containing Bufr message.
-     kelem - An INTEGER variable containing expected number of expanded elements
-     kvals - An INTEGER variable containing expected number of data values.
+   length - An INTEGER variable containing length of Bufr message in words.
+   kbuff - An INTEGER array containing Bufr message.
+   kelem - An INTEGER variable containing expected number of expanded elements
+   kvals - An INTEGER variable containing expected number of data values.
 
    ON OUTPUT:
 
-     KSEC0 - An INTEGER array (size 3) containing Bufr Section 0 information.
-            [0] Length of section 0 in bytes
-            [1] Total length of Bufr message in bytes
-            [2] Bufr Edition number (currently 4)
+   KSEC0 - An INTEGER array (size 3) containing Bufr Section 0 information.
+   [0] Length of section 0 in bytes
+   [1] Total length of Bufr message in bytes
+   [2] Bufr Edition number (currently 4)
 
-     KSEC1 - An INTEGER array of at least 40 words containing Bufr Section 1 information. When Section
-             1 contains data for local use, KSEC1 should be sized accordingly.
-            [0] Length of section 1 in bytes
-            [1] Bufr Edition number (currently 4)
-            [2] Originating centre
-            [3] Update sequence number
-            [4] Flag (presence of Section 2 in the message)
-            [5] Bufr message type ( Bufr Table A)
-            [6] Bufr message subtype (local use)
-            [7] Version number of local table used
-            [8] Year
-            [9] Month
-            [10] Day
-            [11] Hour
-            [12] Minute
-            [13] Bufr Master Table used
-            [14] Version number of Master table used
-            [15] Originating sub-centre
-            [16] International sub-category
-            [17] Second
-            [18-39] Local ADP centre information (byte by byte)
+   KSEC1 - An INTEGER array of at least 40 words containing Bufr Section 1 information. When Section
+   1 contains data for local use, KSEC1 should be sized accordingly.
+   [0] Length of section 1 in bytes
+   [1] Bufr Edition number (currently 4)
+   [2] Originating centre
+   [3] Update sequence number
+   [4] Flag (presence of Section 2 in the message)
+   [5] Bufr message type ( Bufr Table A)
+   [6] Bufr message subtype (local use)
+   [7] Version number of local table used
+   [8] Year
+   [9] Month
+   [10] Day
+   [11] Hour
+   [12] Minute
+   [13] Bufr Master Table used
+   [14] Version number of Master table used
+   [15] Originating sub-centre
+   [16] International sub-category
+   [17] Second
+   [18-39] Local ADP centre information (byte by byte)
 
-     KSEC2 - An INTEGER array of 4096 words containing Bufr Section 2 information. ECMWF uses this
-             section to store Report Data Base KEY.
-             [0] Length of Section 2 in bytes
-             [1-4095] Report Data Base KEY in packed form
+   KSEC2 - An INTEGER array of 4096 words containing Bufr Section 2 information. ECMWF uses this
+   section to store Report Data Base KEY.
+   [0] Length of Section 2 in bytes
+   [1-4095] Report Data Base KEY in packed form
 
-     KSEC3 - An INTEGER array of 4 words containing Bufr Section 3 information.
-             [0] Length of Section 3 in bytes
-             [1] Reserved
-             [2] Number of subsets
-             [3] Flag (data type, compression)
+   KSEC3 - An INTEGER array of 4 words containing Bufr Section 3 information.
+   [0] Length of Section 3 in bytes
+   [1] Reserved
+   [2] Number of subsets
+   [3] Flag (data type, compression)
 
-     KSEC4 - An INTEGER array of 2 words containing Section 4 information.
-             [0] Length of Section 4 in bytes
-             [1] Reserved
+   KSEC4 - An INTEGER array of 2 words containing Section 4 information.
+   [0] Length of Section 4 in bytes
+   [1] Reserved
 
-     KSUP - An INTEGER array (size 9) containing supplementary information.
-             [0] Dimension of KSEC1 array
-             [1] Dimension of KSEC2 array
-             [2] Dimension of KSEC3 array
-             [3] Dimension of KSEC4 array
-             [4] Real number of expanded elements
-             [5] Number of subsets
-             [6] Real number of elements in CVALS array
-             [7] Total Bufr message length in bytes
-             [8] Dimension of KSEC0 array
+   KSUP - An INTEGER array (size 9) containing supplementary information.
+   [0] Dimension of KSEC1 array
+   [1] Dimension of KSEC2 array
+   [2] Dimension of KSEC3 array
+   [3] Dimension of KSEC4 array
+   [4] Real number of expanded elements
+   [5] Number of subsets
+   [6] Real number of elements in CVALS array
+   [7] Total Bufr message length in bytes
+   [8] Dimension of KSEC0 array
 
-     CNAMES - CHARACTER*64 array of KELEM words containing element names.
-     CUNITS - CHARACTER*24 array of KELEM words containing element units.
-     VALUES - REAL*8 array of KVALS words containing element values.
-     CVALS - CHARACTER*80 array of KVALS containing CCITT IA No.5 element entries.
-     KERR - An INTEGER containing an error code.
+   CNAMES - CHARACTER*64 array of KELEM words containing element names.
+   CUNITS - CHARACTER*24 array of KELEM words containing element units.
+   VALUES - REAL*8 array of KVALS words containing element values.
+   CVALS - CHARACTER*80 array of KVALS containing CCITT IA No.5 element entries.
+   KERR - An INTEGER containing an error code.
 
-  */
+   */
 
   KERR = 0;
-  bufrex_ ( &length, ( int * ) kbuff, KSUP, KSEC0, KSEC1, KSEC2, KSEC3, KSEC4,
-            &kelem, ( char ** ) CNAMES, ( char ** ) CUNITS, &kvals,
-            VALUES, ( char ** ) CVALS, &KERR );
-  if ( KERR )
+  bufrex_(&length, (int *) kbuff, KSUP, KSEC0, KSEC1, KSEC2, KSEC3, KSEC4,
+      &kelem, (char **) CNAMES, (char **) CUNITS, &kvals, VALUES,
+      (char **) CVALS, &KERR);
+  if (KERR)
     {
       printf("KERR=%d\n", KERR);
       KERR = 0;
@@ -241,17 +242,17 @@ int main ( int argc, char *argv[] )
   if (VERBOSE)
     {
       /*! Section 2 of the Bufr message is an optional section and every ADP centre can pack
-          any information in this section. The Bufr software decodes this local information
-          and stores it into KSEC2 array. ECMWF is storing RDB key in the Section 2 of the Bufr
-          messages. To print content of the Section 2, subroutine BUUKEY must be called before
-          the BUPRS2 routine. */
-      printf ( "KSEC3[3]=%d\n", KSEC3[2] );
-      buukey_ ( KSEC1, KSEC2, KEY, KSUP, &KERR );
+       any information in this section. The Bufr software decodes this local information
+       and stores it into KSEC2 array. ECMWF is storing RDB key in the Section 2 of the Bufr
+       messages. To print content of the Section 2, subroutine BUUKEY must be called before
+       the BUPRS2 routine. */
+      printf("KSEC3[3]=%d\n", KSEC3[2]);
+      buukey_(KSEC1, KSEC2, KEY, KSUP, &KERR);
 
       /*! Returns lists of unexpanded and expanded data descriptors from the Bufr message.
-          The lists contains Bufr Table D sequence numbers, and the Bufr Table B reference numbers. */
+       The lists contains Bufr Table D sequence numbers, and the Bufr Table B reference numbers. */
 
-      busel_ ( &ktdlen, KTDLST, &ktdexl, KTDEXP, &KERR );
+      busel_(&ktdlen, KTDLST, &ktdexl, KTDEXP, &KERR);
 
 #ifdef DEBUG
       for (i = 0; i < ktdexl; i++)
@@ -261,17 +262,18 @@ int main ( int argc, char *argv[] )
       }
 #endif
       /*! Prints section 3.
-          Prior to calling the BUPRS3 routine, the BUSEL or BUSEL2 routine has to be called to get lists
-          of unexpanded and fully expanded Data descriptors. In the case of multi-subset uncompressed bufr
-          data the expanded list of descriptors might be different for different subsets. */
-      buprs3_ ( KSEC3, &ktdlen, KTDLST, &ktdexl, KTDEXP, &kelem, ( char ** ) CNAMES );
+       Prior to calling the BUPRS3 routine, the BUSEL or BUSEL2 routine has to be called to get lists
+       of unexpanded and fully expanded Data descriptors. In the case of multi-subset uncompressed bufr
+       data the expanded list of descriptors might be different for different subsets. */
+      buprs3_(KSEC3, &ktdlen, KTDLST, &ktdexl, KTDEXP, &kelem,
+          (char **) CNAMES);
 
       /*! Print data */
       icode = 0;
       current_ss = 1;
-      buprt_ ( &icode, &current_ss, &KSEC3[2], &kelem, ( char ** ) CNAMES,
-               ( char ** ) CUNITS, ( char ** ) CVALS,
-               &kvals, VALUES, KSUP, KSEC1, &KERR );
+      buprt_(&icode, &current_ss, &KSEC3[2], &kelem, (char **) CNAMES,
+          (char **) CUNITS, (char **) CVALS, &kvals, VALUES, KSUP, KSEC1,
+          &KERR);
     }
 
 #ifdef DEBUG
@@ -281,13 +283,13 @@ int main ( int argc, char *argv[] )
     printf("KSEC1[%d] = %d\n", i, KSEC1[i]);
   if (KSEC1[4])
     {
-      for (i = 0; i < 4096 ; i++)
+      for (i = 0; i < 4096; i++)
         printf("KSEC2[%d] = %d\n", i, KSEC2[i]);
     }
-  for (i = 0; i < 4 ; i++)
+  for (i = 0; i < 4; i++)
     printf("KSEC3[%d] = %d\n", i, KSEC3[i]);
 
-  for (i = 0; i < 9 ; i++)
+  for (i = 0; i < 9; i++)
     printf("KSUP[%d] = %d\n", i, KSUP[i]);
 #endif
 
