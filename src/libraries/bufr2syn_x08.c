26d125f15c5028229a4f22c15d53a5d377582e93
Guillermo Ballester Valor <gballesterv@aemet.es>
2014-10-03 10:02:51 +0000

    fixed typo in documantation

--- src/libraries/bufr2syn_x08.c
+++ src/libraries/bufr2syn_x08.c
@@ -18,7 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 /*!
- \file bufr2syn_x07.c
+ \file bufr2syn_x08.c
  \brief decodes the descriptors with X = 08 (Significance Qualifiers)
 
   As there is no significance information in synop we use this descriptors as
