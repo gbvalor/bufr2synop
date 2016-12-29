695d78feda54ebe36c63f79cb495bb764f0746e7
Guillermo Ballester valor <gbv@ogimet.com>
2016-12-29 19:09:56 +0000

    added scripts to get bufr tables from WMO files

--- src/bufrdeco/bufrdeco_tableb.c
+++ src/bufrdeco/bufrdeco_tableb.c
@@ -131,6 +131,99 @@ int bufr_read_tableb ( struct bufr_tableb *tb, char *error )
   return 0;
 }
 
+int bufr_read_tableb_csv ( struct bufr_tableb *tb, char *error )
+{
+  char *c;
+  FILE *t;
+  size_t i = 0;
+  int nt;
+  uint32_t ix;
+  char l[256];
+  char caux[256];
+  char *tk[16];
+  struct bufr_descriptor desc;
+
+  if ( tb->path == NULL )
+    {
+      return 1;
+    }
+
+  // If we've already readed this table. We just regenerate the table with original values
+  if ( strcmp ( tb->path, tb->old_path ) == 0 )
+    {
+      //printf ("Reutilizo tablas\n");
+      for ( i = 0; i < tb->nlines ; i++ )
+        {
+          tb->item[i].scale = tb->item[i].scale_ori;
+          tb->item[i].reference = tb->item[i].reference_ori;
+          tb->item[i].nbits = tb->item[i].nbits_ori;
+          tb->item[i].changed = 0;
+        }
+      return 0; // all done
+    }
+
+  strcpy ( caux, tb->path );
+  memset ( tb, 0, sizeof ( struct bufr_tableb ) );
+  strcpy ( tb->path, caux );
+  if ( ( t = fopen ( tb->path, "r" ) ) == NULL )
+    {
+      sprintf ( error,"Unable to open table B file '%s'\n", tb->path );
+      return 1;
+    }
+
+  while ( fgets ( l, 256, t ) != NULL && i < BUFR_MAXLINES_TABLEB )
+    {
+      // Parse line
+      if (parse_csv_line(&nt, tk, l) < 0)
+      {
+        sprintf ( error,"Error parsing csv line from table B file '%s'\n", tb->path );
+        return 1;
+      }
+          
+      // First we build the descriptor
+      ix = strtoul ( tk[0], &c, 10 );
+      uint32_t_to_descriptor ( &desc, ix );
+      tb->item[i].changed = 0; // Original from table B
+      tb->item[i].x = desc.x; // x
+      tb->item[i].y = desc.y; // y
+      strcpy ( tb->item[i].key, desc.c ); // key
+      if ( tb->x_start[desc.x] == 0 )
+        {
+          tb->x_start[desc.x] = i;  // marc the start
+        }
+      tb->y_ref[desc.x][desc.y] = i - tb->x_start[desc.x]; // marc the position from start of first x
+
+      // detailed name
+      if (tk[2][0])
+        sprintf(tb->item[i].name, "%s %s", tk[1], tk[2]);
+      else
+        strcpy(tb->item[i].name, tk[1]);
+      
+      // unit
+      strcpy(tb->item[i].unit, tk[3]);
+
+      // escale
+      tb->item[i].scale_ori = strtol ( tk[4], &c, 10 );
+      tb->item[i].scale = tb->item[i].scale_ori;
+
+      // reference
+      tb->item[i].reference_ori = strtol ( tk[5], &c, 10 );
+      tb->item[i].reference = tb->item[i].reference_ori;
+
+      // bits
+      tb->item[i].nbits_ori = strtol ( tk[6], &c, 10 );
+      tb->item[i].nbits = tb->item[i].nbits_ori;
+
+      i++;
+    }
+  tb->x_start[0] = 0; // fix the start for x = 0
+  fclose ( t );
+  tb->nlines = i;
+  strcpy ( tb->old_path, tb->path ); // store latest path
+  return 0;
+}
+
+
 /*!
   \fn int bufr_restore_original_tableb_item ( struct bufr_tableb *tb, struct bufrdeco *b, uint8_t mode, char *key )
   \brief Restores the original table B parameters for a BUFR descriptor
