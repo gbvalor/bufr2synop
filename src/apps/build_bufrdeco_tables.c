ebe62dcd0b385b479d12c8fb37aa5c8e2d0bcf08
Guillermo Ballester Valor <gbv@ogimet.com>
2017-01-02 23:23:03 +0000

    build_bufrdeco_tables also builds from ECMWF bufr B and D tables

--- src/apps/build_bufrdeco_tables.c
+++ src/apps/build_bufrdeco_tables.c
@@ -28,6 +28,7 @@
 const char SELF[] = "build_bufrdeco_tables";
 char INPUT_FILE[256];
 char TABLE_TYPE[8];
+int IS_WMO;
 int A_FIELDS[8] = {1,2,-1,-1,-1,-1,-1,-1}; // fields selected for table A type
 int B_FIELDS[8] = {3,4,5,6,7,8,9,-1}; // fields selected for table B type
 int C_FIELDS[8] = {1,3,4,5,6,7,-1,-1}; // Fields selected for CodeFlag (C) type.
@@ -37,8 +38,9 @@ void print_usage ( void )
 {
   printf ( "%s %s\n", SELF, PACKAGE_VERSION );
   printf ( "Usage: \n" );
-  printf ( "%s -i input_file [-t table_type][-h]\n" , SELF );
+  printf ( "%s -i input_file -t table_type [-e[-h]\n" , SELF );
   printf ( "       -h Print this help\n" );
+  printf ( "       -e Source ECMWF, default WMO\n" );
   printf ( "       -t table_type. (A = TableA, B = TableB, C = CodeFlag, D = TableD)\n" );
 }
 
@@ -47,14 +49,18 @@ int main ( int argc, char *argv[] )
 {
   int iopt, i, *fs;
   FILE *f;
-  char lin[CSV_MAXL], caux[CSV_MAXL];
-  char *tk[16];
+  char lin[CSV_MAXL], caux[CSV_MAXL], caux2[32];
+  char *tk[16], *c;
   int nt;
+  uint32_t ix;
+  long int il;
+  struct bufr_descriptor desc;
 
   // Read args
   TABLE_TYPE[0] = 0;
   INPUT_FILE[0] = 0;
-  while ( ( iopt = getopt ( argc, argv, "hi:t:" ) ) !=-1 )
+  IS_WMO = 1;
+  while ( ( iopt = getopt ( argc, argv, "hei:t:" ) ) !=-1 )
     switch ( iopt )
       {
       case 'i':
@@ -64,6 +70,10 @@ int main ( int argc, char *argv[] )
       case 't':
         if ( strlen ( optarg ) < 8 )
           strcpy ( TABLE_TYPE, optarg );
+        break;
+      case 'e':
+      case 'E':
+        IS_WMO = 0;
         break;
       case 'h':
       default:
@@ -89,50 +99,122 @@ int main ( int argc, char *argv[] )
       exit ( EXIT_FAILURE );
     }
 
-  switch ( TABLE_TYPE[0] )
+  if ( IS_WMO )
     {
-    case 'A':
-    case 'a':
-      fs = & ( A_FIELDS[0] );
-      break;
-    case 'B':
-    case 'b':
-      fs = & ( B_FIELDS[0] );
-      break;
-    case 'C':
-    case 'c':
-      fs = & ( C_FIELDS[0] );
-      break;
-    case 'D':
-    case 'd':
-      fs = & ( D_FIELDS[0] );
-      break;
-    default:
-      fprintf ( stderr, "%s: Error, bad '%s' table type\n", SELF, TABLE_TYPE );
-      fclose ( f );
-      exit ( EXIT_FAILURE );
-    }
-
-  while ( fgets ( lin, CSV_MAXL, f ) != NULL && i < BUFR_MAXLINES_TABLEB )
-    {
-      // Parse line
-      if ( parse_csv_line ( &nt, tk, lin ) < 0 )
+      switch ( TABLE_TYPE[0] )
         {
-          fprintf ( stderr, "%s: Error parsing csv line '%s' from file '%s'\n", SELF, lin, INPUT_FILE );
+        case 'A':
+        case 'a':
+          fs = & ( A_FIELDS[0] );
+          break;
+        case 'B':
+        case 'b':
+          fs = & ( B_FIELDS[0] );
+          break;
+        case 'C':
+        case 'c':
+          fs = & ( C_FIELDS[0] );
+          break;
+        case 'D':
+        case 'd':
+          fs = & ( D_FIELDS[0] );
+          break;
+        default:
+          fprintf ( stderr, "%s: Error, bad '%s' table type\n", SELF, TABLE_TYPE );
           fclose ( f );
           exit ( EXIT_FAILURE );
         }
 
-      i = 0;
-      while ( fs[i] >= 0 )
+      while ( fgets ( lin, CSV_MAXL, f ) != NULL )
         {
-          if ( i > 0 )
-            printf ( "," );
-          if (strlen(tk[fs[i]]) )
-            printf ( "%s", csv_quoted_string ( caux, tk[fs[i]] ) );
-          i++;
+          // Parse line
+          if ( parse_csv_line ( &nt, tk, lin ) < 0 )
+            {
+              fprintf ( stderr, "%s: Error parsing csv line '%s' from file '%s'\n", SELF, lin, INPUT_FILE );
+              fclose ( f );
+              exit ( EXIT_FAILURE );
+            }
+
+          i = 0;
+          while ( fs[i] >= 0 )
+            {
+              if ( i > 0 )
+                printf ( "," );
+              if ( strlen ( tk[fs[i]] ) )
+                printf ( "%s", csv_quoted_string ( caux, tk[fs[i]] ) );
+              i++;
+            }
+          printf ( "\n" );
         }
-      printf ( "\n" );
+    }
+  else
+    {
+      // first line 
+      printf("\"First line1 with missing info\"\n");  
+      while ( fgets ( lin, CSV_MAXL, f ) != NULL )
+        {
+          // supress the newline
+          if ( ( c = strrchr ( lin,'\n' ) ) != NULL )
+            {
+              *c = '\0';
+            }
+
+          switch ( TABLE_TYPE[0] )
+            {
+            case 'B':
+            case 'b':
+              // Descriptor
+              ix = strtoul ( &lin[0], &c, 10 );
+              uint32_t_to_descriptor ( &desc, ix );
+              printf ( "\"%s\",", desc.c );
+
+              // detailed name
+              bufr_charray_to_string ( caux, &lin[8], 64 );
+              bufr_adjust_string ( caux ); // supress trailing blanks
+              printf ( "\"%s\",", caux );
+
+              // tyoe
+              bufr_charray_to_string ( caux ,&lin[73], 24 );
+              bufr_adjust_string ( caux );
+              printf ( "\"%s\",", caux );
+
+              // escale
+              il = strtol ( &lin[97], &c, 10 );
+              printf ( "\"%ld\",", il );
+
+              // reference
+              il = strtol ( &lin[102], &c, 10 );
+              printf ( "\"%ld\",", il );
+
+              // bits
+              il = strtol ( &lin[115], &c, 10 );
+              printf ( "\"%ld\"\n", il );
+              break;
+
+            case 'C':
+            case 'c':
+              fs = & ( C_FIELDS[0] );
+              break;
+
+            case 'D':
+            case 'd':
+
+              if (lin[1] != ' ')
+              {
+                 memcpy(caux2, lin + 1, 6);
+                 caux2[6] = '\0';
+              }
+              memcpy(caux, lin + 11, 6);
+              caux[6] = '\0';
+              printf("\"%s\",\"%s\"\n", caux2, caux);
+              break;
+
+            default:
+              fprintf ( stderr, "%s: Error, bad '%s' table type\n", SELF, TABLE_TYPE );
+              fclose ( f );
+              exit ( EXIT_FAILURE );
+            }
+        }
     }
   fclose ( f );
   exit ( EXIT_SUCCESS );
