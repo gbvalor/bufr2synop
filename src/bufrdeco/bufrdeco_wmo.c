da1af11f2eb004dca945ccc4e1455535252f08cd
Guillermo Ballester Valor <gbvalor@gmail.com>
2023-07-11 11:02:55 +0000

    Added tables and support for BUFR Version 40_0_0

--- src/bufrdeco/bufrdeco_wmo.c
+++ src/bufrdeco/bufrdeco_wmo.c
@@ -134,6 +134,7 @@ int get_wmo_tablenames ( struct bufrdeco *b )
     case 36:
     case 37:
     case 39:  
+    case 40:  
       snprintf ( b->tables->b.path, sizeof ( b->tables->b.path ),"%sBUFR_%d_0_0_TableB_en.csv", aux, b->sec1.master_version );
       snprintf ( b->tables->c.path, sizeof ( b->tables->c.path ),"%sBUFR_%d_0_0_TableC_en.csv", aux, b->sec1.master_version );
       snprintf ( b->tables->d.path, sizeof ( b->tables->d.path ),"%sBUFR_%d_0_0_TableD_en.csv", aux, b->sec1.master_version );
@@ -144,9 +145,9 @@ int get_wmo_tablenames ( struct bufrdeco *b )
       snprintf ( b->tables->d.path, sizeof ( b->tables->d.path ),"%sBUFR_%d_1_0_TableD_en.csv", aux, b->sec1.master_version );
       break;
     default: // last version for BUFR tables
-      snprintf ( b->tables->b.path, sizeof ( b->tables->b.path ),"%sBUFR_39_0_0_TableB_en.csv", aux );
-      snprintf ( b->tables->c.path, sizeof ( b->tables->c.path ),"%sBUFR_39_0_0_TableC_en.csv", aux );
-      snprintf ( b->tables->d.path, sizeof ( b->tables->d.path ),"%sBUFR_39_0_0_TableD_en.csv", aux );
+      snprintf ( b->tables->b.path, sizeof ( b->tables->b.path ),"%sBUFR_40_0_0_TableB_en.csv", aux );
+      snprintf ( b->tables->c.path, sizeof ( b->tables->c.path ),"%sBUFR_40_0_0_TableC_en.csv", aux );
+      snprintf ( b->tables->d.path, sizeof ( b->tables->d.path ),"%sBUFR_40_0_0_TableD_en.csv", aux );
       break;
     }
   return 0;
